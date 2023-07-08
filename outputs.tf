18bf6d397107732168b22acedd87fdb35daa3704
Oleh Palii <o.palij@gmail.com>
2023-07-08 17:27:16 +0300

    add iam_role_arn/iam_role_name outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "eip_public_address" {
 output "wireguard_port" {
   value = var.wireguard_port
 }
+
+output "iam_role_arn" {
+  value = module.iam_role.iam_role_arn
+}
+
+output "iam_role_name" {
+  value = module.iam_role.iam_role_name
+}
